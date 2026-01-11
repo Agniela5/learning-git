37d7269f1cf824b986e562de76e53e815795b69c
Agnieszka Trela <atrela1987@gmail.com>
2026-01-11 18:08:05 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+shopping_list = {
+    "piekarnia": ["chelb", "bulki", "paczek"],
+    "warzywniak": ["marchew", "seler", "rukola"]
+}
+
+for shop, product in shopping_list.items():
+    product_capital = [p.title() for p in product]
+    product_string = ", ".join(product_capital)
+
+    print(f"Ide do {shop.title()}, kupuje tu nastepujace rzeczy: {product_string}")
+
+total_products = sum(len(product) for product in shopping_list.values())
+print(f"W sumie kupuję {total_products} produktów.")
